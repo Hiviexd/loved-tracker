fd76af569200708e1beab1785f82efdbd77222eb
Hivie <me@hivie.tn>
2025-08-15 19:26:27 +0100

    safer processing

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const SHEET_NAME = config.sheetName;
 const WEBHOOK_URL = config.webhookUrl;
 const DISCORD_PINGS = config.discordPings;
 
+async function timeout(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 async function sendWebhook(row) {
     console.log(`Sending webhook for user ${row[1]}`);
 
@@ -16,7 +20,7 @@ async function sendWebhook(row) {
         .trim();
 
     const embed = {
-        description: `[**${row[1]}**](https://osu.ppy.sh/users/${row[0]}) needs a tenure badge update! :tada: \n\`\`\`${row[4]}\`\`\``,
+        description: `[**${row[1]}**](https://osu.ppy.sh/users/${row[0]}) needs a tenure badge update! **${row[3]} → ${row[2]}** :tada: \n\`\`\`${row[4]}\`\`\``,
         color: parseInt("ff76c0", 16), // #ff76c0
         image: {
             url: `https://assets.ppy.sh/profile-badges/${row[4].split(" ")[2]}`,
@@ -32,7 +36,7 @@ async function sendWebhook(row) {
         console.error("Failed to send webhook: ", error);
     } finally {
         // 1s timeout
-        await new Promise((resolve) => setTimeout(resolve, 1000));
+        await timeout(1000);
     }
 }
 
@@ -43,21 +47,28 @@ async function fetchAndProcessSheet() {
 
     await doc.loadInfo();
 
-    // timeout 15s
-    await new Promise((resolve) => setTimeout(resolve, 15 * 1000));
+    // 15s timeout to give the command cells time to load
+    await timeout(15 * 1000);
 
     const sheet = doc.sheetsByTitle[SHEET_NAME];
 
     const rows = await sheet.getRows();
 
+    // 10s timeout for extra safety
+    await timeout(10 * 1000);
+
     console.log("Processing Project Loved tenures...");
+
+    const blacklistedRowData = ["loading...", "loading", "...", "null", "n/a"];
 
     // start from row 3 (index 2)
     for (let i = 2; i < rows.length; i++) {
         const row = rows[i];
 
-        if (row._rawData[4] !== "...") {
+        if (!blacklistedRowData.includes(row._rawData[4].toLowerCase())) {
             await sendWebhook(row._rawData);
+        } else {
+            console.log(`Skipping row ${i} (user: ${row[1]}) because command cell seems invalid: ${row._rawData[4]}`);
         }
     }
 
