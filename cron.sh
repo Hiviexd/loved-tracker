2e72f1764375f4adc0ef5d264646751a21137ba2
Hivie <me@hivie.tn>
2026-02-03 03:11:23 +0100

    more fixes

--- cron.sh
+++ cron.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 USER_HOME=$HOME
-CURRENT_USER=$(whoami)
 
-export PATH=/usr/local/bin:/usr/bin:/bin:$USER_HOME/.local/share/pnpm:$USER_HOME/.nvm/versions/node/$(node -v 2>/dev/null)/bin:$PATH
+export PATH="$USER_HOME/.local/share/pnpm:/usr/local/bin:/usr/bin:/bin:$PATH"
 
-cd "$USER_HOME/scripts/loved-tracker"
+cd "$USER_HOME/scripts/loved-tracker" || exit
 
-$(command -v pnpm) start >> "$USER_HOME/scripts/loved-tracker/logs/cronjob.log" 2>&1
+echo "--- Job started at $(date) ---" >> "$USER_HOME/scripts/loved-tracker/logs/cronjob.log"
+
+pnpm start >> "$USER_HOME/scripts/loved-tracker/logs/cronjob.log" 2>&1
